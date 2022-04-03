7e4422b16d207800608951840b0a3b9c1041e2de
NonQuant <husainov.21.04@gmail.com>
2022-04-03 14:51:32 +0500

    Add project files.

---
+++ Project 2/Form2.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_2
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private bool finder(char elem, string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] == elem)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const string small = "abcdefghijklmnopqrstuvwxyz", big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+                         num = "1234567890", symbols = "_@#$%!";
+            bool c1 = false, c2 = false, c3 = false, c4 = false;
+            string password = textBox1.Text;
+            if (password == "")
+            {
+                MessageBox.Show("Пожалуйста, введите пароль");
+                return;
+            }
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (finder(password[i], big))
+                {
+                    c1 = true;
+                }
+                if (finder(password[i], small))
+                {
+                    c2 = true;
+                }
+                if (finder(password[i], symbols))
+                {
+                    c3 = true;
+                }
+                if (finder(password[i], num))
+                {
+                    c4 = true;
+                }
+            }
+            label2.BackColor = Color.LightCoral;
+            label2.ForeColor = Color.Black;
+            if (!c1){
+                label2.Text = "В вашем пароле нет заглавной буквы!";
+            }
+
+            else if (!c2){
+                label2.Text = "В вашем пароле нет строчной буквы!";
+            }
+
+            else if (!c4){
+                label2.Text = "В вашем пароле нет цифры!";
+            }
+
+            else if (!c3){
+                label2.Text = "В вашем пароле нет другого символа!";
+            }
+
+            else if (password.Length < 10)
+            {
+                label2.Text = "Ваш пароль слишком короткий!(должно быть 10 символов)";
+            }
+            else
+            {
+                label2.Text = "Ваш пароль надежный!";
+                label2.BackColor = Color.LimeGreen;
+            }
+            label2.Visible = true;
+
+        }
+    }
+}
---
+++ Project 2/Form3.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project_2
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+        private bool finder(char elem, string password)
+        {
+            for (int i = 0; i < password.Length; i++)
+            {
+                if (password[i] == elem)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string all = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890_@#$%!";
+            string small = "abcdefghijklmnopqrstuvwxyz", big = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            string nums = "1234567890", symbols = "_@#$%!";
+            string a = "";
+            char elem;
+            int elem_num;
+            Random random = new Random(DateTime.Now.Second);
+
+            elem_num = random.Next(0, big.Length);
+            elem = big[elem_num];
+            a = a + elem;
+
+            elem_num = random.Next(0, small.Length);
+            elem = small[elem_num];
+            a = a + elem;
+
+            elem_num = random.Next(0, symbols.Length);
+            elem = symbols[elem_num];
+            a = a + elem;
+
+            elem_num = random.Next(0, nums.Length);
+            elem = nums[elem_num];
+            a = a + elem;
+
+            for (int i = 0; i < 6; i++)
+            {
+                elem_num = random.Next(0, all.Length);
+                elem = all[elem_num];
+                a = a + elem;
+            }
+            textBox1.Text = a;
+            textBox1.Visible = true;
+            label1.Visible = true;
+        }
+    }
+}
